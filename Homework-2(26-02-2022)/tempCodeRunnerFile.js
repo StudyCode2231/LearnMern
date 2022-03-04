5f0dd0bb14a1c79d908022b5f14eb17d4019ef44
StudyCode2231 <1903044@kiit.ac.in>
2022-03-04 09:46:28 +0530

    file update

--- Homework-2(26-02-2022)/tempCodeRunnerFile.js
+++ Homework-2(26-02-2022)/tempCodeRunnerFile.js
@@ -1,4 +1,1 @@
-const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"]; 
-fruits.splice(1, 3,"Grapes","Pear","Watermelon","Muskmelon","Kiwi");
-fruits.unshift("0th");
-console.log(fruits);
+Object.freeze(person);
---
+++ JS/closure.js
@@ -0,0 +1,9 @@
+function outer(){
+    var a=10;
+    function inner(){
+        console.log(a);
+    }
+    return inner;
+}
+var z=outer();
+console.log(z);
