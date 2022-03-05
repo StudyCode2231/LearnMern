cd37ebc6315e10125cf6ba5f964bc0db8fd1ba35
StudyCode2231 <1903044@kiit.ac.in>
2022-03-05 16:36:41 +0530

    file update

--- Homework-2(26-02-2022)/question.js
+++ Homework-2(26-02-2022)/question.js
@@ -409,11 +409,11 @@
 
 // // < --------------Question 30----------------->
 
-// var num = 8;
-// var num = 10;
+var num = 8;
+var num = 10;
 
-// console.log(num);
-// A: 8
-// B: 10
-// C: SyntaxError
-// D: ReferenceError
+console.log(num);
+A: 8
+B: 10
+C: SyntaxError
+D: ReferenceError
